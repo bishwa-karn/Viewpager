a6e8c8c8b89eb2188f11b76420fe9882a3323a23
bishwa-karn <atish.karn@gmail.com>
2016-12-06 08:12:26 +0545

    first commit

---
+++ AndroidStudioProjects/FragmentSecondDemo/app/src/main/java/com/example/bishwa/karn/fragmentseconddemo/FragmentLogin.java
@@ -0,0 +1,37 @@
+package com.example.bishwa.karn.fragmentseconddemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by acer on 11/27/2016.
+ */
+
+public class FragmentLogin extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_login,container,false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
